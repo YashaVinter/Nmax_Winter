d16569a91adbc559c968aa20709306049531b55a
yasha <vinter-jasha@rambler.ru>
2023-02-21 22:29:50 +0500

    fix case with 1 nmax

--- lib/nmax_winter/nmax.rb
+++ lib/nmax_winter/nmax.rb
@@ -36,10 +36,11 @@ module NmaxWinter
         end
   
         if [" ", "\n", "\t"].include?(ch)
-          number_i = number.to_i if is_number && number.size != 0 && number != "-"
-          # не убирется мин число
-          set << number_i if number_i && (set.empty? || set.count < n || number_i < set.min)
-  
+          number_i = number.to_i if is_number && number.size != 0 && number.size <= 1000 && number != "-"
+          set << number_i if number_i && (set.empty? || set.count < n || number_i > set.min)
+          if set.count > n
+            set.delete(set.min)
+          end
           number = ""
           is_number = true
           minus = false
@@ -61,9 +62,11 @@ module NmaxWinter
         # other cases
         is_number = false
       end
-      number_i = number.to_i if is_number && number.size != 0 && number != "-"
-      set << number_i if number_i && (set.empty? || set.count < n || number_i < set.min)
-  
+      number_i = number.to_i if is_number && number.size != 0 && number.size <= 1000 && number != "-"
+      set << number_i if number_i && (set.empty? || set.count < n || number_i > set.min)
+      if set.count > n
+        set.delete(set.min)
+      end
       set.to_a.join(" ")
     end
   end
